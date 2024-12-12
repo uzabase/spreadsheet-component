d1cf2211714f10340fa4cea10397646cee7b3219
naoki.tomita <mxcpy608@hotmail.com>
2024-12-12 16:12:31 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,14 @@ export default defineConfig({
   build: {
     lib: {
       entry: "./index.tsx",
-      formats: ["iife"],
+      formats: ["umd"],
       fileName(_, __) {
         return "spreadsheet.js";
       },
       name: "spreadsheet",
-    }
-  }
+    },
+  },
+  define: {
+    "process.env.NODE_ENV": `"production"`,
+  },
 });
