ddfd7d7baa0d23a78e5421683c31f0b86ccd4ae3
naoki.tomita <mxcpy608@hotmail.com>
2025-02-03 21:38:45 +0900

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     lib: {
       entry: "./index.tsx",
       formats: ["umd"],
-      fileName(_, __) {
+      fileName() {
         return "spreadsheet.js";
       },
       name: "spreadsheet",
